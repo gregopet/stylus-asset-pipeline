568d36d0d4b2d8ab2b0dfd1924dc9e9f06e18513
gregorp <gregap@gmail.com>
2014-10-24 09:41:04 +0200

    Added a working testing environment config

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,4 +1,22 @@
+grails.servlet.version = "3.0" // Change depending on target container compliance (2.5 or 3.0)
 grails.project.work.dir = 'target'
+grails.project.class.dir = "target/classes"
+grails.project.dependency.resolver = "maven" // or ivy
+
+grails.project.fork = [
+    // configure settings for compilation JVM, note that if you alter the Groovy version forked compilation is required
+    //  compile: [maxMemory: 256, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
+
+    // configure settings for the test-app JVM, uses the daemon by default
+    test: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
+    // configure settings for the run-app JVM
+    run: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
+    // configure settings for the run-war JVM
+    war: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
+    // configure settings for the Console UI JVM
+    console: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256]
+]
+
 grails.project.dependency.resolution = {
 	
 	inherits 'global'
@@ -11,6 +29,7 @@ grails.project.dependency.resolution = {
 	}
 	
 	dependencies {
+		runtime 'org.springframework:spring-expression:4.0.5.RELEASE'
 	}
 
 	plugins {
@@ -19,5 +38,9 @@ grails.project.dependency.resolution = {
 		build(":release:3.0.1", ":rest-client-builder:1.0.3") {
 			export = false
 		}
+		
+		//testing required classes
+		build ":tomcat:7.0.54"
+		compile ':cache:1.1.7'
 	}
 }
